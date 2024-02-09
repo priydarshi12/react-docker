2c344f085aa4f7e0b7e4f6142b26d7028fb655e2
priydarshi12 <21001016054.jcboseust.ac.in>
2024-02-09 14:53:53 +0530

    basic template

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+ 
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h1 class="text-3xl font-bold text-black underline">this is my text</h1>
+    </>
+  )
+}
+
+export default App
